5e6cbed4654547299758b82d331b1459488f1311
mayankkumar <mayankkumar0078@gmail.com>
2022-10-05 08:46:11 +0530

    -plus one

---
+++ src/main/java/javageeksforgeeks/leetcode/easy/PlusOne.java
@@ -0,0 +1,13 @@
+package javageeksforgeeks.leetcode.easy;
+
+public class PlusOne {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public int[] plusOne(int[] digits) {
+//        for(int i=digits.length-1;i>=0;i--) {
+//
+//        }
+//    }
+}
